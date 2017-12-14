5640e7ff0e43db596da4f158abe7c7ba374a8b01
javawww <1003632308@qq.com>
2017-12-14 10:29:39 +0800

    bbbb

---
+++ githello/src/com/git/pojo/User.java
@@ -0,0 +1,54 @@
+package com.git.pojo;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+	private String username;
+	private String password;
+	private Integer age;
+	/**
+	 * @return the username
+	 */
+	public String getUsername() {
+		return username;
+	}
+	/**
+	 * @param username the username to set
+	 */
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	/**
+	 * @return the age
+	 */
+	public Integer getAge() {
+		return age;
+	}
+	/**
+	 * @param age the age to set
+	 */
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	
+	
+}
